43da1d80fc331549ff29d25eb5c7203e84bd534a
MikeKadoshnikov <35706715+MikeKadoshnikov@users.noreply.github.com>
2023-01-07 12:11:08 -0700

    Genesis v1.0
    Genesis Project Test v1.0
    -Added Cube to Pygame
    -Added Breathing effect to cube on mouse click, need to figure music out
    -Added shift in x axis to cube on up arrow need to figure out music
    -Have media player from pygame but needs to be integrated into cube
    
    Project seems to currently be pheasible!

---
+++ test.py
@@ -0,0 +1,62 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
+from pygame import mixer
+
+class MP:
+    def __init__(self, win):
+        win.geometry('400x600')
+        win.title('Music Player')
+        win.resizable(0, 0)
+
+        self.play_restart = tk.StringVar()
+        self.pause_resume = tk.StringVar()
+
+        self.play_restart.set('Play')
+        self.pause_resume.set('Pause')
+
+        load_button = Button(win, text='Load', width=10, font=('Arial', 20), command=self.load)
+        load_button.place(x=100, y=50, anchor='center')
+
+        play_button = Button(win, textvariable=self.play_restart, width=10, font=('Arial', 20), command=self.play)
+        play_button.place(x=100, y=100, anchor='center')
+
+        pause_button = Button(win, textvariable=self.pause_resume, width=10, font=('Arial', 20), command=self.pause)
+        pause_button.place(x=100, y=150, anchor='center')
+
+        stop_button = Button(win, text='Stop', width=10, font=('Arial', 20), command=self.stop)
+        stop_button.place(x=100, y=200, anchor='center')
+
+        self.music_file = False
+        self.playing_state = False
+        
+    def load(self):
+        self.music_file = filedialog.askopenfilename()
+        print('Loaded: ', self.music_file)
+        self.play_restart.set('Play')
+
+    def play(self):
+        if self.music_file:
+            mixer.init()
+            mixer.music.load(self.music_file)
+            mixer.music.play()
+            self.playing_state = False
+            self.play_restart.set('Restart')
+            self.pause_resume.set('Pause')
+
+    def pause(self):
+        if not self.playing_state:
+            mixer.music.pause()
+            self.playing_state = True
+            self.pause_resume.set('Resume')
+        else:
+            mixer.music.unpause()
+            self.playing_state = False
+            self.pause_resume.set('Pause')
+
+    def stop(self):
+        mixer.music.stop()
+
+root = Tk()
+MP(root)
+root.mainloop()

cf9db8be33c16d0dd110e0bf50a50db353e6b3ba
MikeKadoshnikov <35706715+MikeKadoshnikov@users.noreply.github.com>
2023-01-16 18:10:57 -0700

    Genesis v1.03 added new graph & fixed wav bits
    -Added second graph semilog better for visualizer data than the first graph
    -Got the wav to work with struct unpack, wav uses 32 bit structure
    -Played around with the chunk size can be changed to whatever but found:
                 -1024 is too fast and high freq
                 -2048 is a little too slow but some lower freq appear
                 -4096 is way slower but more bassy freq appear in the wave function
                 -8192+ gets much slower but bassier waveform is generated
    
    -visualizer has been reduced in function as to work out the graphs first
    -multithreading still is eluding not included for this version
    
    Personal Notes:
    Significant progress has been made if I got paid for this job to sit here listen to music and get data to visualize it, damn that would be great :)
    
    - Mike Kadoshnikov

---
+++ Genesis v1.03/main.py
@@ -0,0 +1,210 @@
+import pygame
+import os
+import math
+import time
+from matrix import *
+
+os.environ["SDL_VIDEO_CENTERED"]='1'
+width, height = 1920, 1080
+black, white, blue = (20, 20, 20), (230, 230, 230), (0, 154, 255)
+
+#pygame
+pygame.init()
+pygame.display.set_caption("3D Cube")
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+fps = 300
+
+
+angle = 0
+cube_position = [width//2, height//2]
+scale = 600
+speed = 0.005
+
+#iterating through all the points change to something else if needed
+points = [n for n in range(8)]
+
+neg_one = -1
+one = 1
+
+points[0] = [[neg_one], [neg_one], [one]]
+points[1] = [[one], [neg_one], [one]]
+points[2] = [[one], [one], [one]]
+points[3] = [[neg_one], [one], [one]]
+points[4] = [[one], [neg_one], [neg_one]]
+points[5] = [[one], [neg_one], [neg_one]]
+points[6] = [[one], [one], [neg_one]]
+points[7] = [[neg_one], [one], [neg_one]]
+
+pt0_x = neg_one
+pt1_x = one
+pt2_x = one
+pt3_x = neg_one
+pt4_x = neg_one
+pt5_x = one
+pt6_x = one
+pt7_x = neg_one
+
+def connect_point(i, j, k):
+    a = k[i]
+    b = k[j]
+    pygame.draw.line(screen, black, (a[0], a[1]), (b[0], b[1]), 4)
+
+
+def toRun(x_temp, y_temp, x_dir_change):
+    global angle
+    global neg_one
+    global one
+    global pt0_x
+    global pt1_x
+    global pt2_x
+    global pt3_x
+    global pt4_x
+    global pt5_x
+    global pt6_x
+    global pt7_x
+    index = 0
+    one += x_temp
+    neg_one -= y_temp
+
+    ##print(str(one) + " " + str(neg_one))
+    pt0_x += x_dir_change - y_temp
+    pt1_x += x_dir_change + x_temp
+    pt2_x += x_dir_change + x_temp
+    pt3_x += x_dir_change - y_temp
+    pt4_x += x_dir_change - y_temp
+    pt5_x += x_dir_change + x_temp
+    pt6_x += x_dir_change + x_temp
+    pt7_x += x_dir_change - y_temp
+    
+    points[0] = [[pt0_x], [neg_one], [one]]
+    points[1] = [[pt1_x], [neg_one], [one]]
+    points[2] = [[pt2_x], [one], [one]]
+    points[3] = [[pt3_x], [one], [one]]
+    points[4] = [[pt4_x], [neg_one], [neg_one]]
+    points[5] = [[pt5_x], [neg_one], [neg_one]]
+    points[6] = [[pt6_x], [one], [neg_one]]
+    points[7] = [[pt7_x], [one], [neg_one]]
+        
+    projected_points = [j for j in range(len(points))]
+
+    rotation_x = [[1,0,0],
+                 [0, math.cos(angle), -math.sin(angle)],
+                 [0, math.sin(angle), math.cos(angle)]]
+
+    rotation_y = [[math.cos(angle), 0, -math.sin(angle)],
+                 [0, 1, 0],
+                 [math.sin(angle), 0, math.cos(angle)]]
+
+    rotation_z = [[math.cos(angle), -math.sin(angle), 0],
+                 [math.sin(angle), math.cos(angle), 0],
+                 [0, 0, 1]]
+
+    for point in points:
+        rotated_2d = matrix_multiplication(rotation_y, point)
+        rotated_2d = matrix_multiplication(rotation_x, rotated_2d)
+        rotated_2d = matrix_multiplication(rotation_z, rotated_2d)
+
+        distance = 5
+
+        z = 1/(distance - rotated_2d[2][0])
+        projection_matrix = [[z, 0, 0],
+                            [0, z, 0]]
+        projected2d = matrix_multiplication(projection_matrix, rotated_2d)
+
+        x = int(projected2d[0][0] * scale) + cube_position[0]
+        y = int(projected2d[1][0] * scale) + cube_position[1]
+
+        projected_points[index] = [x, y]
+        pygame.draw.circle(screen, blue, (x,y), 10)
+        index += 1
+
+    for m in range(4):
+        connect_point(m, (m+1)%4, projected_points)
+        connect_point(m+4, (m+1)%4+4, projected_points)
+        connect_point(m, m+4, projected_points)
+
+    angle += speed
+    pygame.display.update()
+
+######
+def RunAnimation(value):
+    count = 0
+    count_2 = 0
+    clock.tick(fps)
+    screen.fill(white)
+
+    ##Runs the base case cube with only rotation and not modifications
+    toRun(0, 0, 0)
+
+    ##Runs the breathing of the cube
+    if value > 2000000000:
+        if(count < 2):
+            count+=1
+
+    if(count == 1):
+        for i in range(5):
+            toRun(0.01, 0.01, 0)
+            screen.fill(white)
+
+        for i in range(5):
+            toRun(-0.01, -0.01, 0)
+            screen.fill(white)
+            count = 0
+##    if(count == 1 and count_2 < 50):
+##        toRun(0.02, 0.02, 0)
+##        screen.fill(white)
+##        count_2 += 1
+##        if(count_2 == 50):
+##            count = 2
+##            count_2 = 100
+##
+##    if(count == 2 and count_2 > 50):
+##        toRun(-0.02, -0.02, 0)
+##        screen.fill(white)
+##        count_2 -= 1
+##        if(count_2 == 50):
+##            count = 0
+##            count_2 = 0
+
+##    ##Runs the x axis shift in the cube
+##    if event.type == pygame.KEYDOWN:
+##        if(event.key == pygame.K_UP):
+##            if(count_2 < 2):
+##                count_2+=1
+##        
+##    if(count_2 > 0):
+##        for i in range(50):
+##            toRun(0, 0, i/200)
+##            screen.fill(white)
+##
+##        for i in range(50):
+##            toRun(0, 0, -i/100)
+##            screen.fill(white)
+##
+##        for i in range(50):
+##            toRun(0, 0, i/200)
+##            screen.fill(white)
+##            count_2 = 0
+##
+##
+##    ##Runs the x axis shift and the breathing at the same time
+##    if (event.type == pygame.KEYDOWN):
+##        if(event.key == pygame.K_DOWN):
+##            if((count < 2) and (count_2 < 2)):
+##                count = 5
+##                count_2 = 5
+##
+##    if((count == 5) and (count_2 == 5)):
+##        for i in range(50):
+##            toRun(0.02, 0.02, i/400)
+##            screen.fill(white)
+##
+##        for i in range(50):
+##            toRun(-0.02, -0.02, -i/400)
+##            screen.fill(white)
+##            count = 0
+##            count_2 = 0
+
+
+
